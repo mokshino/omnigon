a3575abeb9dd746a5560541b44e5a735ddc88acc
Anton Mokshyn <mokshino@gmail.com>
2018-08-30 00:02:32 +0300

    addded spring boot application entrypoint class

---
+++ rest/src/main/java/com/omnigon/hometest/rest/controller/PingRest.java
@@ -0,0 +1,17 @@
+package com.omnigon.hometest.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Anton Mokshyn.
+ */
+@RestController
+public class PingRest {
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return "OK!";
+    }
+
+}
